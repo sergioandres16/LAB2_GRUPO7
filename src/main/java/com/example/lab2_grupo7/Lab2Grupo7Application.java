b23f114b33ecf1371e1a24ce24676b9b9e18b876
Sergio <sergio.sotomayor@pucp.edu.pe>
2022-04-12 19:25:53 -0500

    primer commit

---
+++ src/main/java/com/example/lab2_grupo7/Lab2Grupo7Application.java
@@ -0,0 +1,13 @@
+package com.example.lab2_grupo7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab2Grupo7Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab2Grupo7Application.class, args);
+    }
+
+}

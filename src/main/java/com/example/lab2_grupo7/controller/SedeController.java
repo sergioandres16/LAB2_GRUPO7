b2b0ad8c83f73671d5be0922b42b72ca1e3becc8
Ronny Pastor <ronnypastorkolmakov@gmail.com>
2022-04-12 23:57:25 -0500

    Vistas list, edit y create de sedes. Asimismo, vista list de trabajadores

--- src/main/java/com/example/lab2_grupo7/controller/SedeController.java
+++ src/main/java/com/example/lab2_grupo7/controller/SedeController.java
@@ -4,12 +4,14 @@ import com.example.lab2_grupo7.entity.Sede;
 import com.example.lab2_grupo7.entity.Trabajador;
 import com.example.lab2_grupo7.repository.SedeRepository;
 import com.example.lab2_grupo7.repository.TrabajadorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 import java.util.Optional;
@@ -17,7 +19,11 @@ import java.util.Optional;
 @Controller
 @RequestMapping("/sedes")
 public class SedeController {
+
+    @Autowired
     SedeRepository sedeRepository;
+
+    @Autowired
     TrabajadorRepository trabajadorRepository;
 
     @GetMapping("/lista")
@@ -33,7 +39,12 @@ public class SedeController {
     }
 
     @PostMapping("/guardar")
-    public String guardarSede(Sede sede){
+    public String guardarSede(Sede sede, RedirectAttributes attributes, @RequestParam("action") int action){
+        if (action == 1){
+            attributes.addFlashAttribute("msg", "Sede eliminada exitosamente");
+        }else if (action == 2){
+            attributes.addFlashAttribute("msg", "Sede eliminada exitosamente");
+        }
         sedeRepository.save(sede);
         return "redirect:/sedes/lista";
     }
@@ -50,7 +61,18 @@ public class SedeController {
         }else{
             return "redirect:/sedes/lista";
         }
+    }
 
+    //Falta modificar por que hay trabajadores dentro de ellas, primero se deberia eliminar los trabajadores
+    @GetMapping("/borrar")
+    public String borrarSede(RedirectAttributes attributes, @RequestParam("id") int id){
+        Optional<Sede> optionalSede = sedeRepository.findById(id);
+
+        if (optionalSede.isPresent()) {
+            sedeRepository.deleteById(id);
+            attributes.addFlashAttribute("msg", "Sede eliminada exitosamente");
+        }
+        return "redirect:/shipper/list";
     }
 
 }
--- src/main/java/com/example/lab2_grupo7/controller/TrabajadorController.java
+++ src/main/java/com/example/lab2_grupo7/controller/TrabajadorController.java
@@ -4,12 +4,14 @@ import com.example.lab2_grupo7.entity.Sede;
 import com.example.lab2_grupo7.entity.Trabajador;
 import com.example.lab2_grupo7.repository.SedeRepository;
 import com.example.lab2_grupo7.repository.TrabajadorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 import java.util.Optional;
@@ -18,7 +20,9 @@ import java.util.Optional;
 @RequestMapping("/trabajador")
 public class TrabajadorController {
 
+    @Autowired
     TrabajadorRepository trabajadorRepository;
+    @Autowired
     SedeRepository sedeRepository;
 
     @GetMapping("/lista")
@@ -56,6 +60,13 @@ public class TrabajadorController {
 
     }
 
-
-
+    @GetMapping("/borrar")
+    public String borrarSede(RedirectAttributes attributes, @RequestParam("id") String id){
+        Optional<Trabajador> optionalTrabajador = trabajadorRepository.findById(id);
+        if (optionalTrabajador.isPresent()) {
+            trabajadorRepository.deleteById(id);
+            attributes.addFlashAttribute("msg", "Trabajador eliminado exitosamente");
+        }
+        return "redirect:/shipper/list";
+    }
 }
